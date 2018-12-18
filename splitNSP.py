109dd75c8d95685319e5ab5172d377f20a708d85
Jeremy Amorin <jamorin@users.noreply.github.com>
2018-12-17 18:30:55 -0700

    Change NSP output to .ns0 .ns1 .ns2

--- splitNSP.py
+++ splitNSP.py
@@ -12,68 +12,6 @@ startTime = datetime.now()
 splitSize = 0xFFFF0000 # 4,294,901,760 bytes
 chunkSize = 0x8000 # 32,768 bytes
 
-def splitQuick(filepath):
-    fileSize = os.path.getsize(filepath)
-    info = shutil.disk_usage(os.path.dirname(os.path.abspath(filepath)))
-    if info.free < splitSize:
-        print('Not enough temporary space. Needs 4GiB of free space\n')
-        return
-    print('Calculating number of splits...\n')
-    splitNum = int(fileSize/splitSize)
-    if splitNum == 0:
-        print('This NSP is under 4GiB and does not need to be split.\n')
-        return
-
-    print('Splitting NSP into {0} parts...\n'.format(splitNum + 1))
-
-    # Create directory, delete if already exists
-    dir = filepath[:-4] + '_split.nsp'
-    if os.path.exists(dir):
-        shutil.rmtree(dir)
-    os.makedirs(dir)
-
-    # Move input file to directory and rename it to first part
-    filename = os.path.basename(filepath)
-    shutil.move(filepath, os.path.join(dir, '00'))
-    filepath = os.path.join(dir, '00')
-
-    # Calculate size of final part to copy first
-    finalSplitSize = fileSize - (splitSize * splitNum)
-
-    # Copy final part and trim from main file
-    with open(filepath, 'r+b') as nspFile:
-        nspFile.seek(finalSplitSize * -1, os.SEEK_END)
-        outFile = os.path.join(dir, '{:02}'.format(splitNum))
-        partSize = 0
-        print('Starting part {:02}'.format(splitNum))
-        with open(outFile, 'wb') as splitFile:
-            while partSize < finalSplitSize:
-                splitFile.write(nspFile.read(chunkSize))
-                partSize += chunkSize
-        nspFile.seek(finalSplitSize * -1, os.SEEK_END)
-        nspFile.truncate()
-        print('Part {:02} complete'.format(splitNum))
-
-    # Loop through additional parts and trim
-    with open(filepath, 'r+b') as nspFile:
-        for i in range(splitNum - 1):
-            nspFile.seek(splitSize * -1, os.SEEK_END)
-            outFile = os.path.join(dir, '{:02}'.format(splitNum - (i + 1)))
-            partSize = 0
-            print('Starting part {:02}'.format(splitNum - (i + 1)))
-            with open(outFile, 'wb') as splitFile:
-                 while partSize < splitSize:
-                    splitFile.write(nspFile.read(chunkSize))
-                    partSize += chunkSize
-            nspFile.seek(splitSize * -1, os.SEEK_END)
-            nspFile.truncate()
-            print('Part {:02} complete'.format(splitNum - (i + 1)))
-    
-    # Print assurance statement for user
-    print('Starting part 00\nPart 00 complete')
-
-    print('\nNSP successfully split!\n')
-    
 def splitCopy(filepath):
     fileSize = os.path.getsize(filepath)
     info = shutil.disk_usage(os.path.dirname(os.path.abspath(filepath)))
@@ -85,24 +23,25 @@ def splitCopy(filepath):
     if splitNum == 0:
         print('This NSP is under 4GiB and does not need to be split.\n')
         return
-    
+
     print('Splitting NSP into {0} parts...\n'.format(splitNum + 1))
 
     # Create directory, delete if already exists
-    dir = filepath[:-4] + '_split.nsp'
-    if os.path.exists(dir):
-        shutil.rmtree(dir)
-    os.makedirs(dir)
+    dir = filepath[:-4]
 
     remainingSize = fileSize
+
+    print('\n'+ filepath)
 
     # Open source file and begin writing to output files stoping at splitSize
     with open(filepath, 'rb') as nspFile:
         for i in range(splitNum + 1):
             partSize = 0
             print('Starting part {:02}'.format(i))
-            outFile = os.path.join(dir, '{:02}'.format(i))
-            with open(outFile, 'wb') as splitFile: 
+            # basename = os.path.splitext(os.path.basename(dir))[0]
+            outFile = dir + '.ns' + str(i)
+            print('\n'+ outFile)
+            with open(outFile, 'wb') as splitFile:
                 if remainingSize > splitSize:
                     while partSize < splitSize:
                         splitFile.write(nspFile.read(chunkSize))
@@ -121,7 +60,6 @@ def main():
     # Arg parser for program options
     parser = argparse.ArgumentParser(description='Split NSP files into FAT32 compatible sizes')
     parser.add_argument('filepath', help='Path to NSP file')
-    parser.add_argument('-q', '--quick', action='store_true', help='Splits file in-place without creating a copy. Only requires 4GiB free space to run')
 
     # Check passed arguments
     args = parser.parse_args()
@@ -132,12 +70,8 @@ def main():
     if os.path.isfile(filepath) == False:
         print('NSP cannot be found\n')
         return 1
-    
-    # Split NSP file
-    if args.quick:
-        splitQuick(filepath)
-    else:
-        splitCopy(filepath)
+
+    splitCopy(filepath)
 
 if __name__ == "__main__":
     main()
